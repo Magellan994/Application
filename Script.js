8eb7821da8c299286d4b6b134e47cb89fa0c0c65
Magellan994 <khukha994@gmail.com>
2023-05-13 19:14:54 +0300

    App

---
+++ Script.js
@@ -0,0 +1,23 @@
+$(function() {
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+        items:1,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
+    });
+});
+
+
+$(function(){
+    $('.repeat').click(function(){
+        var classes =  $(this).parent().attr('class');
+        $(this).parent().attr('class', 'animate');
+        var indicator = $(this);
+        setTimeout(function(){
+            $(indicator).parent().addClass(classes);
+        }, 20);
+    });
+});
